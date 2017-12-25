f2c57826c4f36d4a2e8c610c7c48f6366c774f57
yewenhe0904 <yewenhe0904@gmail.com>
2017-12-25 10:33:24 -0600

    separate page to display blogs

--- mblog/mainsite/views.py
+++ mblog/mainsite/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from datetime import datetime
 from django.template.loader import get_template
@@ -10,3 +11,15 @@ def homepage(request):
 	posts=Post.objects.all()
 	html=template.render(locals())
 	return HttpResponse(html)
+
+
+def showpost(request,slug):
+	template=get_template('post.html')
+	try:
+		post=Post.objects.get(slug=slug) ##fetch the post corresponds to the slug
+		now=datetime.now()
+		if post !=None:	
+			html=template.render(locals())
+			return HttpResponse(html)
+	except:
+		return redirect('/')
